2d9912f5c8644c49071080b13791dac28642102f
vennam himabindu <vennamhimabindu27@gmail.com>
2025-07-08 23:31:25 +0530

    fix: corrected /api/auth route mounting

--- index.js
+++ index.js
@@ -13,11 +13,10 @@ app.use(cors());
 app.use(express.json());
 
 const authRoutes = require("./routes/authRoutes");
-app.use("/api", authRoutes); 
+app.use("/api/auth", authRoutes); 
 app.get("/", (req, res) => {
   res.send("✅ Backend is working");
 });
-
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("✅ MongoDB Connected");
